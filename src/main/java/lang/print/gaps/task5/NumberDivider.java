7868f465396d4f15a772231a774c275810bef7b7
Dominykas Anuzis <dominykasanuzis@gmail.com>
2022-07-13 11:24:35 +0300

    done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    void divide(double toBeDivided) {
+        toBeDivided /= 5;
+        System.out.println(toBeDivided);
     }
 }
